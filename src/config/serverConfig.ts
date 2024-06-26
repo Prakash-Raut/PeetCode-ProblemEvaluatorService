95efb0f6336a6c892cb29603ca56b60848d74368
Prakash Raut <prakash_raut@outlook.com>
2024-06-26 20:41:04 +0530

    added redis bullmq and created sample job and queue

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -3,5 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 5005;
+const REDIS_PORT = parseInt(process.env.REDIS_PORT || "6379", 10);
+const REDIS_HOST = process.env.REDIS_HOST || "127.0.0.1";
 
-export { PORT };
+export { PORT, REDIS_HOST, REDIS_PORT };
