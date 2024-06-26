a12e47db8519d5ad5b25062448da4093785f9372
Prakash Raut <prakash_raut@outlook.com>
2024-06-26 21:40:38 +0530

    add bullmq producer and worker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { type Express } from "express";
 import { PORT } from "./config/serverConfig";
+import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
+import SampleWorker from "./workers/sampleWorker";
 
 const app: Express = express();
 
@@ -8,4 +10,13 @@ app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port: ${PORT}`);
+});
+
+SampleWorker("SampleQueue");
+
+sampleQueueProducer("SampleJob", {
+	name: "Sanket",
+	company: "Microsoft",
+	position: "SDE 2 L61",
+	location: "Remote | BLR | Noida",
 });
--- src/jobs/sampleJob.ts
+++ src/jobs/sampleJob.ts
@@ -1,5 +1,5 @@
-import { Job } from "bullmq";
-import { IJob } from "../types/bullMqJobDefinition";
+import type { Job } from "bullmq";
+import type { IJob } from "../types/bullMqJobDefinition";
 
 export default class SampleJob implements IJob {
 	name: string;
@@ -10,8 +10,12 @@ export default class SampleJob implements IJob {
 		this.name = this.constructor.name;
 	}
 
-	handle = () => {
+	handle = (job?: Job) => {
 		console.log("Handler of the job called");
+		console.log(this.payload);
+		if (job) {
+			console.log(job.name, job.id, job.data);
+		}
 	};
 
 	failed = (job?: Job): void => {
